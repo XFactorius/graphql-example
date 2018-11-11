ca79fba49ea8a34f2b28ed2738e5f38b224910f1
XFactor <tekorius.mantas@gmail.com>
2018-11-11 20:46:08 +0200

    Removed not used imports

--- src/main/java/com/example/graphql/GraphqlApplication.java
+++ src/main/java/com/example/graphql/GraphqlApplication.java
@@ -1,14 +1,12 @@
 package com.example.graphql;
 
-import com.example.graphql.repository.PeopleRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class GraphqlApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GraphqlApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlApplication.class, args);
+    }
 }
