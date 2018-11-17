902b8330418e8cba6688fd1a9453456c6b84ca7c
XFactor <tekorius.mantas@gmail.com>
2018-11-17 20:56:44 +0200

    Added lombok to project

--- src/main/java/com/example/graphql/model/People.java
+++ src/main/java/com/example/graphql/model/People.java
@@ -1,10 +1,13 @@
 package com.example.graphql.model;
 
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Setter
 @Entity(name = "peoples")
 public class People {
 
@@ -13,20 +16,4 @@ public class People {
     private Long id;
 
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
